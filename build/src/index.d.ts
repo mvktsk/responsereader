0172febb915ec2db08d62fcf091b5a214252e27a
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2022-07-25 13:35:39 +0700

    feat: add loop for every 1 minute
    Init project
    Added loop for 1 minute
    Added time format
    Added quit from process whem q is pressed

---
+++ build/src/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="node" />
+declare const minutes = 1;
+declare const timeOut: number;
+declare const serversArray: string[];
+declare let isQuit: boolean;
+declare const readline: any;
+declare const handle: NodeJS.Timeout;
+declare function roundToNearestMinute(date?: Date): string;
